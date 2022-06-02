5b07171e4a4153040b692b9ac6d9171d45b75e60
junseopark <junseopark0331@icloud.com>
2022-06-02 22:47:34 +0900

    Initial Commit

---
+++ counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  counter
+//
+//  Created by 박준서 on 2022/06/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ counter/counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterApp.swift
+//  counter
+//
+//  Created by 박준서 on 2022/06/02.
+//
+
+import SwiftUI
+
+@main
+struct counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
